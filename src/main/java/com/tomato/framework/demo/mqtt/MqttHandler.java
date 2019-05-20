a03d7fc0e4e7957858a463737029e8668e10a816
gerry <yuanguohua_7818@163.com>
2019-05-20 22:36:55 +0800

    优化mqtt监听者代码

--- src/main/java/com/tomato/framework/demo/mqtt/MqttHandler.java
+++ src/main/java/com/tomato/framework/demo/mqtt/MqttHandler.java
@@ -1,7 +1,6 @@
 package com.tomato.framework.demo.mqtt;
 
 import com.tomato.framework.plugin.mqtt.invoke.AbstractMqttInvoke;
-import com.tomato.framework.plugin.mqtt.invoke.MqttInvokePlugin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +10,11 @@ public class MqttHandler extends AbstractMqttInvoke {
     
     @Override
     public void invoke(String msg) {
-        log.info("消息接收"+msg);
+        log.info("消息接收" + msg);
     }
     
     @Override
-    public void afterPropertiesSet() throws Exception {
-        MqttInvokePlugin.getInstance().store(new String[]{"topic1"}, this);
+    public String[] listenTopics() {
+        return new String[]{"topic"};
     }
 }
