33b0d52c60c051e03d64b02ae9d89d7e2ea33eae
yuanguohua <yuanguohua@fin-shine.com>
2019-05-21 13:17:53 +0800

    mqtt代码优化

--- src/main/java/com/tomato/framework/demo/mqtt/MqttHandler.java
+++ src/main/java/com/tomato/framework/demo/mqtt/MqttHandler.java
@@ -1,21 +1,21 @@
 package com.tomato.framework.demo.mqtt;
 
 import com.tomato.framework.plugin.mqtt.invoke.AbstractMqttInvoke;
-import com.tomato.framework.plugin.mqtt.invoke.MqttInvokePlugin;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-public class MqttHandler extends AbstractMqttInvoke {
+public class MqttHandler extends AbstractMqttInvoke<String> {
     
     @Override
-    public void invoke(String msg) {
-        log.info("消息接收"+msg);
+    public String[] listenTopics() {
+        return new String[]{"topic1"};
     }
     
+    
     @Override
-    public void afterPropertiesSet() throws Exception {
-        MqttInvokePlugin.getInstance().store(new String[]{"topic1"}, this);
+    public void handler(String msg) {
+        System.out.println(msg);
     }
 }
